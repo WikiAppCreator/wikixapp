38b904eb2030d24fc1b4ab6d2a7b51463085cb24
WikiAppCreator <95003626+WikiAppCreator@users.noreply.github.com>
2022-06-02 10:00:41 +0800

    Update wiki.sh

--- wiki.sh
+++ wiki.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 /usr/local/bin/v2ray -config /config.json &
+/bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
 cd /wiki
 node server
